f7dc2189fe6fb45133b0b7cae6b6a5c07994dca5
Hunc Tasci <htasci@apple.com>
2024-06-05 18:19:59 +0300

    implemented error handling and geocoding

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -6,24 +8,24 @@ const BootcampSchema = new mongoose.Schema({
     required: [true, "Please add a name"],
     unique: true,
     trim: true,
-    maxlength: [50, "Name can not be more than 50 characters."],
+    maxlength: [50, "Name can not be more than 50 characters"],
   },
   slug: String,
   description: {
     type: String,
     required: [true, "Please add a description"],
-    maxlength: [500, "Description can not be more than 500 characters."],
+    maxlength: [500, "Description can not be more than 500 characters"],
   },
   website: {
     type: String,
     match: [
       /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-      "Please use a valid URL with HTTP or HTTPS.",
+      "Please use a valid URL with HTTP or HTTPS",
     ],
   },
   phone: {
     type: String,
-    maxlength: [20, "Phone number can not be longer than 20 characters."],
+    maxlength: [20, "Phone number can not be longer than 20 characters"],
   },
   email: {
     type: String,
@@ -34,18 +36,16 @@ const BootcampSchema = new mongoose.Schema({
   },
   address: {
     type: String,
-    required: [true, "Please add an address."],
+    required: [true, "Please add an address"],
   },
   location: {
     // GeoJSON Point
     type: {
       type: String, // Don't do `{ location: { type: String } }`
       enum: ["Point"], // 'location.type' must be 'Point'
-      required: true,
     },
     coordinates: {
       type: [Number],
-      required: true,
       index: "2dsphere",
     },
     formattedAddress: String,
@@ -70,8 +70,8 @@ const BootcampSchema = new mongoose.Schema({
   },
   averageRating: {
     type: Number,
-    min: [1, "Rating must be at least 1."],
-    max: [10, "Rating must not be more than 10."],
+    min: [1, "Rating must be at least 1"],
+    max: [10, "Rating must not be more than 10"],
   },
   averageCost: Number,
   photo: {
@@ -86,6 +86,31 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+//Geocode & create location field
+BootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  // Do not save address in DB
+  this.address = undefined;
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
